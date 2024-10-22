55ee9134f4904507800e499877a3387bb5e74ca8
Harsh Sharma <121617099+Harsh07Sharma@users.noreply.github.com>
2024-10-22 23:15:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-let taskbar = document.getElementsByClassName("taskbar")[0]
-let startmenu = document.getElementsByClassName("startmenu")[0]
+let taskbar = document.getElementsByClassName("taskbar")[0];
+let startmenu = document.getElementsByClassName("startmenu")[0];
 
-taskbar.addEventListener("click", ()=>{
-    console.log("clicked");
-    if(startmenu.style.bottom == "50px"){
-        startmenu.style.bottom = "-655px"
+// Function to toggle the start menu
+taskbar.addEventListener("click", () => {
+    if (startmenu.style.bottom == "50px") {
+        closeStartMenu();
+    } else {
+        openStartMenu();
     }
-    else{
-        startmenu.style.bottom = "50px"
+});
+
+// Function to open the start menu
+function openStartMenu() {
+    startmenu.style.bottom = "50px";
+    startmenu.style.opacity = "1"; 
+    taskbar.classList.add("active"); 
+}
+
+// Function to close the start menu
+function closeStartMenu() {
+    startmenu.style.bottom = "-655px";
+    startmenu.style.opacity = "0"; 
+    taskbar.classList.remove("active"); 
+}
+
+// Close start menu if user clicks outside of it
+document.addEventListener("click", (event) => {
+    if (!taskbar.contains(event.target) && !startmenu.contains(event.target)) {
+        closeStartMenu();
     }
-})
+});
